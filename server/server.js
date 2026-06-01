c10a14dc6b8a0acf1772211391bb6e91e954f453
sebas313313313 <sebas313313313@users.noreply.github.com>
2026-05-31 23:21:55 -0500

    Fix catch-all regex for Express 5

--- server/server.js
+++ server/server.js
@@ -67,7 +67,7 @@ app.use('/api/announcements', require('./routes/announcementRoutes'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-  app.get('(.*)', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
   });
 }
